ae57472b9ac3ece6b06b35ae4152b1d7c3efaaa4
Aryan Tripathi <48065489+ultrasmartary@users.noreply.github.com>
2020-05-24 05:57:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imagesliderwithswipe/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.imagesliderwithswipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager=findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+}
